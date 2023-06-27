5dc4be5cdfbb1c94e6173a1a1887120873001b2e
elialombardo <elialombardo95@gmail.com>
2023-06-27 15:16:55 +0000

    gitignore added

--- reset_model_and_data_dir.sh
+++ reset_model_and_data_dir.sh
@@ -1,6 +1,5 @@
 rm -rf /workspace/apps/radiology/model/segmentation_2d
 rm /workspace/apps/radiology/model/segmentation_2d.pt
-
 studies_dir='/workspace/images/preprocessed/V002/'
 #rm -rf ${studies_dir}/labels
 rm ${studies_dir}/.lock
