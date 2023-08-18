d9efdd4ff27df00ee0dc70e7936072d4af0d3515
elialombardo <elialombardo95@gmail.com>
2023-08-18 08:46:31 +0000

    changed start server to include other flags

--- monailabel_start_server.sh
+++ monailabel_start_server.sh
@@ -7,7 +7,7 @@ pat_dir="$(basename "$study_dir")"
 mkdir /workspace/apps/radiology/model/"${pat_dir}/"
 
 # Start monai label server for a cine MRI specified by study_dir
-monailabel start_server --app /workspace/apps/radiology --studies ${study_dir} --conf models segmentation_2d
+monailabel start_server --app /workspace/apps/radiology --studies ${study_dir} --conf models segmentation_2d --conf skip_scoring false --conf skip_strategies false --conf epistemic_enabled true
 
 
 
